fb939a90ab2247cc876192edac2b090724f2a633
jasoneczek <github@jasonschaufele.com>
2024-03-22 20:28:56 -0500

    Add functions for populating the display
    This commit adds functionality for populating the calculator display when numbers are clicked, functionality for deleting the last number, clearing the display completely, and toggling the current number negative/positive. This commit contains minor refractoring of HTML and CSS.

--- script.js
+++ script.js
@@ -5,6 +5,56 @@ const operators = {
   '/': divide
 }
 
+const numberBtns = document.querySelectorAll('.btn-number');
+const clearBtn = document.querySelector('#clearBtn');
+const deleteBtn = document.querySelector('#deleteBtn');
+const plusMinusBtn = document.querySelector('#plusMinusBtn');
+const equalsBtn = document.querySelector('#btn-equals');
+const topNumber = document.querySelector('#topNumber');
+const bottomNumber = document.querySelector('#bottomNumber');
+
+
+function displayNumbers() {
+  numberBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      let number = btn.textContent;
+      bottomNumber.textContent += number;
+    });
+  })
+}
+
+
+function clearDisplay() {
+  clearBtn.addEventListener('click', () => {
+    topNumber.textContent = '';
+    bottomNumber.textContent = '';
+  })
+}
+
+function deleteNumber() {
+  deleteBtn.addEventListener('click', () => {
+    let value = bottomNumber.textContent;
+    let newValue; 
+    if (value !== '') {
+      newValue = value.slice(0, -1);
+      bottomNumber.textContent = newValue;
+    }
+  })
+}
+
+function toggleNegative() {
+  plusMinusBtn.addEventListener('click', () => {
+    let value = bottomNumber.textContent;
+    let newValue;
+    if (value.charAt(0) === '-') {
+      newValue = value.slice(1);
+    } else {
+      newValue = `-${value}`;
+    }
+    bottomNumber.textContent = newValue;
+  })
+}
+
 // Basic math operators
 function add(a, b) {
   return a + b;
@@ -29,5 +79,10 @@ function operate(operator, num1, num2) {
   if (Object.hasOwn(operators, operator)) {
     return operators[operator](num1, num2);
   }
-  
 }
+
+displayNumbers();
+clearDisplay();
+deleteNumber();
+toggleNegative();
+
